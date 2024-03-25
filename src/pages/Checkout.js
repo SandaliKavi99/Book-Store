28bec58ff7392e4ebd45d877386cef2450f16407
Sandali Kavishka <103349227+SandaliKavi99@users.noreply.github.com>
2024-03-25 09:37:30 +0530

    changes aws services

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from "../components/CheckoutForm";
 
 const Checkout = () => {
-    const stripePromise = loadStripe('<stripe_public_key>');
+    const stripePromise = loadStripe('pk_test_51OxSTySIgQaki9BWZN1mugQWBnKS0u1liUjx7yB5jyD3MM3KbHR2kRkNBxaF6fFNOZcykOEVMmyvZ6xQ6bQ2DgyN00NgIY40Go');
 
     return (
         <section className="checkout-wrapper">
